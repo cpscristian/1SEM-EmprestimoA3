a3419afe222b946fbe23c260d8b31d549aec164a
Cristian de Paula Santos <1072415373@UNS11DT098793.animaedu.intranet>
2024-05-14 10:25:41 -0300

    feat: Adiciona atributos, construtores e getters/setters

--- src/main/java/model/Amigo.java
+++ src/main/java/model/Amigo.java
@@ -1,5 +1,32 @@
 package model;
 
 public class Amigo {
+    private String nome;
+    private String telefone;
+
+    public Amigo(){
+        this("","");
+    }
+    
+    public Amigo(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
     
 }
