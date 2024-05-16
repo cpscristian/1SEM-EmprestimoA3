ab5b4600cd1cbca095b581975563e058568bc53e
Cristian de Paula Santos <>
2024-05-16 17:41:38 -0300

    feat: gets, sets e construtores para a classe Emprestimo

--- src/main/java/model/Emprestimo.java
+++ src/main/java/model/Emprestimo.java
@@ -1,5 +1,32 @@
 package model;
 
 public class Emprestimo {
-    
+    private String dataInicio;
+    private String dataDevolucao;
+
+    public Emprestimo(){
+        this("","");
+    }
+
+    public Emprestimo(String dataInicio, String dataDevolucao) {
+        this.dataInicio = dataInicio;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public String getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
 }
