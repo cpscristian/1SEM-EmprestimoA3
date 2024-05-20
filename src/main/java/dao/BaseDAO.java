5098358c930ca997b72daefec5683b81f3048063
Cristian de Paula Santos <1072415373@UNS11DT099126.animaedu.intranet>
2024-05-20 10:27:56 -0300

    feat: dependencia do BD e algumas linhas cinzas excluidas

--- src/main/java/dao/BaseDAO.java
+++ src/main/java/dao/BaseDAO.java
@@ -1,5 +1,38 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class BaseDAO {
+    public Connection getConexao() {
     
+        Connection connection = null; //instância da conexão
+            try {
+
+                String driver = "com.mysql.cj.jdbc.Driver";
+                Class.forName(driver);
+
+                String server = "localhost";
+                String database = "db_alunos";
+                String url = "jdbc:mysql://" + server + ":3306/" + database + "?useTimezone=true&serverTimezone=UTC";
+                String user = "root";
+                String password = "root";
+    
+                connection = DriverManager.getConnection(url, user, password);
+
+                if (connection != null) {
+                    System.out.println("Status: Conectado!");
+                } else {
+                    System.out.println("Status: NÃO CONECTADO!");
+                }
+                return connection;
+            } catch (ClassNotFoundException e) { //Driver não encontrado
+                System.out.println("O driver nao foi encontrado.");
+                return null;
+            } catch (SQLException e) {
+                System.out.println("Nao foi possivel conectar...");
+                return null;
+            }
+    }
 }
