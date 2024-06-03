33372d9bc18a3da0522c31fde2b1a40c9001ff7b
Cristian de Paula Santos <102593063+cpscristian@users.noreply.github.com>
2024-06-03 00:34:21 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,34 +1,41 @@
 CREATE TABLE IF NOT EXISTS `emprestimoa3`.`ferramenta` (
   `idFerramenta` INT NOT NULL AUTO_INCREMENT,
-  `nomeFerramenta` VARCHAR(45) NULL,
-  `marca` VARCHAR(45) NULL,
-  `preco` DOUBLE NULL,
+  `nomeFerramenta` VARCHAR(45) NULL DEFAULT NULL,
+  `marca` VARCHAR(45) NULL DEFAULT NULL,
+  `preco` DOUBLE NULL DEFAULT NULL,
   PRIMARY KEY (`idFerramenta`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci
 
 CREATE TABLE IF NOT EXISTS `emprestimoa3`.`amigo` (
   `idAmigo` INT NOT NULL AUTO_INCREMENT,
-  `nomeAmigo` VARCHAR(45) NULL,
-  `telefone` VARCHAR(50) NULL,
+  `nomeAmigo` VARCHAR(45) NULL DEFAULT NULL,
+  `telefone` VARCHAR(50) NULL DEFAULT NULL,
   PRIMARY KEY (`idAmigo`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 14
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci
 
 CREATE TABLE IF NOT EXISTS `emprestimoa3`.`emprestimo` (
-  `ferramenta_idFerramenta` INT NOT NULL,
-  `amigo_idAmigo` INT NOT NULL,
-  `dataInicio` DATE NULL,
-  `dataDevolucao` DATE NULL,
-  PRIMARY KEY (`ferramenta_idFerramenta`, `amigo_idAmigo`),
-  INDEX `fk_ferramenta_has_amigo_amigo1_idx` (`amigo_idAmigo` ASC) VISIBLE,
-  INDEX `fk_ferramenta_has_amigo_ferramenta_idx` (`ferramenta_idFerramenta` ASC) VISIBLE,
-  CONSTRAINT `fk_ferramenta_has_amigo_ferramenta`
-    FOREIGN KEY (`ferramenta_idFerramenta`)
-    REFERENCES `emprestimoa3`.`ferramenta` (`idFerramenta`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  `idEmprestimo` INT NOT NULL AUTO_INCREMENT,
+  `idAmigoEmprestimo` INT NOT NULL,
+  `idFerramentaEmprestimo` INT NOT NULL,
+  `dataInicio` DATE NULL DEFAULT NULL,
+  `dataDevolucao` DATE NULL DEFAULT NULL,
+  `status` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`idEmprestimo`),
+  INDEX `fk_ferramenta_has_amigo_amigo1_idx` (`idAmigoEmprestimo` ASC) VISIBLE,
+  INDEX `fk_ferramenta_has_amigo_ferramenta_idx` (`idFerramentaEmprestimo` ASC) VISIBLE,
   CONSTRAINT `fk_ferramenta_has_amigo_amigo1`
-    FOREIGN KEY (`amigo_idAmigo`)
-    REFERENCES `emprestimoa3`.`amigo` (`idAmigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    FOREIGN KEY (`idAmigoEmprestimo`)
+    REFERENCES `emprestimoa3`.`amigo` (`idAmigo`),
+  CONSTRAINT `fk_ferramenta_has_amigo_ferramenta`
+    FOREIGN KEY (`idFerramentaEmprestimo`)
+    REFERENCES `emprestimoa3`.`ferramenta` (`idFerramenta`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci
