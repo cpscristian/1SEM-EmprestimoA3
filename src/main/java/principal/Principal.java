e2fa32509d0f692a050d08f7b0ba13b76726927c
Luiz <Luiz@N0b3s50P>
2024-06-05 22:11:41 -0300

    Docs: Documentação da classe Principal

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -5,7 +5,9 @@ import visao.MenuPrincipal;
 public class Principal {
 
     public static void main(String[] args) {
+        // Inicializa o programa
         MenuPrincipal janela = new MenuPrincipal();
+        // Torna a janela do menu principal visível para o usuário
         janela.setVisible(true);
     }
 }
