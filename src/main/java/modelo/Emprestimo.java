2dd87b243cf2622fb1fa61759b19105aa2712ab2
Luiz <Luiz@N0b3s50P>
2024-06-05 21:22:11 -0300

    Docs: Documentação na classe Emprestimo.

--- src/main/java/modelo/Emprestimo.java
+++ src/main/java/modelo/Emprestimo.java
@@ -18,6 +18,7 @@ public class Emprestimo {
         this(0,0,0,LocalDate.of(1970, 1, 1),LocalDate.of(1970, 1, 1),false);
     }
 
+    // Construtor parametrizado que inicializa os atributos com valores fornecidos
     public Emprestimo(int idEmprestimo, int idAmigoEmprestimo, int idFerramentaEmprestimo, LocalDate dataInicio, LocalDate dataDevolucao, boolean status) {
         this.idEmprestimo = idEmprestimo;
         this.idAmigoEmprestimo = idAmigoEmprestimo;
@@ -28,50 +29,64 @@ public class Emprestimo {
         this.dao = new EmprestimoDAO();
     }
     
+    // Métodos Getters e Setters
+    
+    // Obtém o ID do empréstimo
     public int getIdEmprestimo() {
         return idEmprestimo;
     }
     
+    // Define o ID do empréstimo
     public void setIdEmprestimo(int idEmprestimo) {
         this.idEmprestimo = idEmprestimo;
     }
     
+    // Obtém o ID do amigo que fez o empréstimo
     public int getIdAmigoEmprestimo() {
         return idAmigoEmprestimo;
     }
     
+    // Define o ID do amigo que fez o empréstimo
     public void setIdAmigoEmprestimo(int idAmigoEmprestimo) {
         this.idAmigoEmprestimo = idAmigoEmprestimo;
     }
     
+    // Obtém o ID da ferramenta emprestada
     public int getIdFerramentaEmprestimo() {
         return idFerramentaEmprestimo;
     }
 
+    // Define o ID da ferramenta emprestada
     public void setIdFerramentaEmprestimo(int idFerramentaEmprestimo) {
         this.idFerramentaEmprestimo = idFerramentaEmprestimo;
     }
     
+    // Obtém a data de início do empréstimo
     public LocalDate getDataInicio() {
         return dataInicio;
     }
 
+    // Define a data de início do empréstimo
     public void setDataInicio(LocalDate dataInicio) {
         this.dataInicio = dataInicio;
     }
 
+    // Obtém a data de devolução do empréstimo
     public LocalDate getDataDevolucao() {
         return dataDevolucao;
     }
 
+    // Define a data de devolução do empréstimo
     public void setDataDevolucao(LocalDate dataDevolucao) {
         this.dataDevolucao = dataDevolucao;
     }
     
+    // Verifica se o empréstimo está ativo
     public boolean isStatus() {
         return status;
     }
 
+    // Define o status do empréstimo
     public void setStatus(boolean status) {
         this.status = status;
     }
@@ -87,6 +102,7 @@ public class Emprestimo {
         return dao.getMinhaListaEmprestimo();
     }
     
+    // Insere um novo empréstimo no banco de dados
     public boolean insertEmprestimoBD(int idAmigoEmprestimo, int idFerramentaEmprestimo, LocalDate dataInicio, LocalDate dataDevolucao) {
         int idEmprestimo = this.maiorIDEmprestimo() + 1;
         Emprestimo objeto = new Emprestimo(idEmprestimo, idAmigoEmprestimo, idFerramentaEmprestimo, dataInicio, dataDevolucao, status);
@@ -94,20 +110,24 @@ public class Emprestimo {
         return true;
     }
     
+    // Obtém o maior ID de empréstimo existente
     public int maiorIDEmprestimo() {
         return dao.maiorIDEmprestimo();
     }
     
+    // Deleta um empréstimo pelo seu ID
     public boolean deleteEmprestimoBD(int idEmprestimo) {
         dao.deleteEmprestimoBD(idEmprestimo);
         return true;
     }
     
+    // Atualiza o status de um empréstimo pelo seu ID
     public boolean updateStatusEmprestimoBD(int idEmprestimo, boolean status){
         dao.updateStatusEmprestimoBD(idEmprestimo, status);
         return true;
     }
     
+    // Verifica se o amigo possui algum empréstimo ativo
     public boolean verificaSeTemEmprestimoAtivo(int idAmigoEmprestimo){
         dao.verificaSeTemEmprestimoAtivo(idAmigoEmprestimo);
         return true;
